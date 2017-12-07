9a92f58befb5d4ccfeff5ad30b026bf1d292cbcd
Rohan Bhattacharjee <rohan.bhattacharjee@oracle.com>
2017-12-06 16:36:27 -0800

    Adding facility to find all the files with Resource and DataSource schema definitions. It is not able to parse all the manual code as yet

--- codeParser.go
+++ codeParser.go
@@ -8,7 +8,7 @@ import (
 	"strconv"
 )
 
-func myParseSchema(fileName string, functionName string) (schemaDefs []SchemaDef) {
+func parseSchema(fileName string, funcNamesWithSchemaDefs map[string]bool) (schemaDefs []SchemaDef) {
 	fset := token.NewFileSet()
 	f, err := parser.ParseFile(fset, fileName, nil, 0)
 
@@ -20,9 +20,10 @@ func myParseSchema(fileName string, functionName string) (schemaDefs []SchemaDef
 
 	// Uncomment the statement above to see the abstract syntax tree (AST) for the code.
 	// Once you see the AST, the following code will make sense.
+	// Logic: Looks for a function in the current file which will have a schema definition. Various correctness assumptions (in the input file) are assumed here.
 	for _, decl := range f.Decls {
 		if function, ok := decl.(*ast.FuncDecl); ok {
-			if function.Name.String() == functionName {
+			if _, exists := funcNamesWithSchemaDefs[function.Name.String()]; exists {
 				var elements = function.Body.List[0].(*ast.ReturnStmt).Results[0].(*ast.UnaryExpr).X.(*ast.CompositeLit).Elts
 
 				for _, elem := range elements {
@@ -34,33 +35,89 @@ func myParseSchema(fileName string, functionName string) (schemaDefs []SchemaDef
 							var nameOfField = schemaElem.(*ast.KeyValueExpr).Key.(*ast.BasicLit).Value
 							schemaDefs[i].FieldName = nameOfField
 
-							for _, typeInfo := range schemaElem.(*ast.KeyValueExpr).Value.(*ast.CompositeLit).Elts {
-								switch typeInfo.(*ast.KeyValueExpr).Key.(*ast.Ident).Name {
-								case "Type":
-									dataTypePart1 := typeInfo.(*ast.KeyValueExpr).Value.(*ast.SelectorExpr).X.(*ast.Ident).Name
-									dataTypePart2 := typeInfo.(*ast.KeyValueExpr).Value.(*ast.SelectorExpr).Sel.Name
-									schemaDefs[i].DataType = fmt.Sprintf("%s.%s", dataTypePart1, dataTypePart2)
+							// E.g. DataSources often have Filters. Schema definitions seem to have <key=Filter, Value=FunctionName>. Handling that here.
+							if _, isCustomFuncCall := schemaElem.(*ast.KeyValueExpr).Value.(*ast.CallExpr); !isCustomFuncCall {
+								for _, typeInfo := range schemaElem.(*ast.KeyValueExpr).Value.(*ast.CompositeLit).Elts {
+									switch typeInfo.(*ast.KeyValueExpr).Key.(*ast.Ident).Name {
+									case "Type":
+										dataTypePart1 := typeInfo.(*ast.KeyValueExpr).Value.(*ast.SelectorExpr).X.(*ast.Ident).Name
+										dataTypePart2 := typeInfo.(*ast.KeyValueExpr).Value.(*ast.SelectorExpr).Sel.Name
+										schemaDefs[i].DataType = fmt.Sprintf("%s.%s", dataTypePart1, dataTypePart2)
 
-								case "Computed":
-									schemaDefs[i].IsComputed, _ = strconv.ParseBool(typeInfo.(*ast.KeyValueExpr).Value.(*ast.Ident).Name)
+									case "Computed":
+										schemaDefs[i].IsComputed, _ = strconv.ParseBool(typeInfo.(*ast.KeyValueExpr).Value.(*ast.Ident).Name)
 
-								case "Required":
-									schemaDefs[i].IsRequired, _ = strconv.ParseBool(typeInfo.(*ast.KeyValueExpr).Value.(*ast.Ident).Name)
+									case "Required":
+										schemaDefs[i].IsRequired, _ = strconv.ParseBool(typeInfo.(*ast.KeyValueExpr).Value.(*ast.Ident).Name)
 
-								case "Optional":
-									schemaDefs[i].IsOptional, _ = strconv.ParseBool(typeInfo.(*ast.KeyValueExpr).Value.(*ast.Ident).Name)
+									case "Optional":
+										schemaDefs[i].IsOptional, _ = strconv.ParseBool(typeInfo.(*ast.KeyValueExpr).Value.(*ast.Ident).Name)
 
-								case "ForceNew":
-									schemaDefs[i].IsForceNew, _ = strconv.ParseBool(typeInfo.(*ast.KeyValueExpr).Value.(*ast.Ident).Name)
+									case "ForceNew":
+										schemaDefs[i].IsForceNew, _ = strconv.ParseBool(typeInfo.(*ast.KeyValueExpr).Value.(*ast.Ident).Name)
+
+									default:
+										fmt.Println("\t****Unknown TypeInfo****")
+										fmt.Printf("\tNameOfSchemaField: %s, Attribute: %s\n", nameOfField, typeInfo.(*ast.KeyValueExpr).Key.(*ast.Ident).Name)
+										fmt.Println("\t*********")
+										fmt.Println()
+									}
 								}
 							}
 						}
 					}
 				}
-
 			}
 		}
 	}
 
 	return
+}
+
+func getDataSourceFuncNames(fileName string, functionName string) map[string]bool {
+	funcNames := make(map[string]bool)
+
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, fileName, nil, 0)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for _, decl := range f.Decls {
+		if function, ok := decl.(*ast.FuncDecl); ok && function.Name.String() == functionName {
+			funcMap := function.Body.List[0].(*ast.ReturnStmt).Results[0].(*ast.CompositeLit).Elts
+
+			for _, mapEntry := range funcMap {
+				dataSourceFuncName := mapEntry.(*ast.KeyValueExpr).Value.(*ast.CallExpr).Fun.(*ast.Ident).Name
+				funcNames[dataSourceFuncName] = false
+			}
+		}
+	}
+
+	return funcNames
+}
+
+func getResourceFuncNames(fileName string, functionName string) map[string]bool {
+	funcNames := make(map[string]bool)
+
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, fileName, nil, 0)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for _, decl := range f.Decls {
+		if function, ok := decl.(*ast.FuncDecl); ok && function.Name.String() == functionName {
+			funcMap := function.Body.List[0].(*ast.ReturnStmt).Results[0].(*ast.CompositeLit).Elts
+
+			for _, mapEntry := range funcMap {
+				resourceFuncName := mapEntry.(*ast.KeyValueExpr).Value.(*ast.CallExpr).Fun.(*ast.Ident).Name
+				funcNames[resourceFuncName] = false
+			}
+		}
+	}
+
+	return funcNames
 }
--- main.go
+++ main.go
@@ -2,17 +2,70 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Analyzing code ...")
 
-	fileName := "./otherSrc/someCode.go"
-	functionName := "ConsoleHistoryResource"
+	var sourceDir = "/Users/rohabhat/Documents/work/code/go/src/github.com/oracle/terraform-provider-oci/provider"
 
-	schemaDefs := myParseSchema(fileName, functionName)
+	var funcNamesWithSchemaDefs = readProviderForFuncNamesWithSchemaDefs(sourceDir)
 
-	for _, schemaDef := range schemaDefs {
-		fmt.Printf("Name: %s Type: %s isRequired: %v isOptional: %v isComputed: %v isForceNew: %v\n", schemaDef.FieldName, schemaDef.DataType, schemaDef.IsRequired, schemaDef.IsOptional, schemaDef.IsComputed, schemaDef.IsForceNew)
+	files, err := ioutil.ReadDir(sourceDir)
+	if err != nil {
+		panic("Could not read source directory")
 	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		if !isResourceFile(file.Name()) && !isDataSourceFile(file.Name()) {
+			continue
+		}
+
+		filePath := fmt.Sprintf("%s/%s", sourceDir, file.Name())
+
+		fmt.Printf("File: %s:\n", file.Name())
+
+		// functionName := "ConsoleHistoryDataDatasource" //"ConsoleHistoryResource"
+		schemaDefs := parseSchema(filePath, funcNamesWithSchemaDefs)
+
+		for _, schemaDef := range schemaDefs {
+			fmt.Printf("\tName: %s Type: %s isRequired: %v isOptional: %v isComputed: %v isForceNew: %v\n", schemaDef.FieldName, schemaDef.DataType, schemaDef.IsRequired, schemaDef.IsOptional, schemaDef.IsComputed, schemaDef.IsForceNew)
+		}
+
+		fmt.Println()
+	}
+}
+
+// This function basically looks that two functions defined in the provider.go, parses a map to get the list of functions that have schemas defined.
+func readProviderForFuncNamesWithSchemaDefs(sourceDir string) map[string]bool {
+	providerFileName := fmt.Sprintf("%s/%s", sourceDir, "provider.go")
+
+	dataSourceFuncList := getDataSourceFuncNames(providerFileName, "dataSourcesMap")
+	resourceFuncList := getResourceFuncNames(providerFileName, "resourcesMap")
+
+	funcNames := make(map[string]bool)
+
+	for k, v := range dataSourceFuncList {
+		funcNames[k] = v
+	}
+
+	for k, v := range resourceFuncList {
+		funcNames[k] = v
+	}
+
+	return funcNames
+}
+
+func isResourceFile(fileName string) bool {
+	return strings.HasSuffix(fileName, "_resource.go")
+}
+
+func isDataSourceFile(fileName string) bool {
+	return strings.HasSuffix(fileName, "_data_source.go")
 }
